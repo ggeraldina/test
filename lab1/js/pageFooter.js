74d90484f06ae3ed9b46c2c10b673978bd90fecf
Geraldina <grushevskaya.d.v@yandex.ru>
2018-03-26 17:21:58 +0000

    Added a footer

---
+++ lab1/js/pageFooter.js
@@ -0,0 +1,15 @@
+// подвал сайта
+
+document.write('<div class="pageFooter">');
+document.write('<div>');
+document.write('<div>');
+document.write('© Грушевская Дарья Владимировна<br>ЯрГУ ИВТ ИТ-31БО,<br>2018'); 
+document.write('</div>');
+document.write('</div>');
+document.write('</div>');        
+        
+// <div class="pageFooter">
+//     <div>
+//       © Грушевская Дарья Владимировна<br>ЯрГУ ИВТ ИТ-31БО,<br>2018 
+//     </div>
+// </div>
