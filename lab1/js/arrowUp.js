a615130cba854f959ee439e55368c02c7283cf9c
Geraldina <grushevskaya.d.v@yandex.ru>
2018-03-26 18:21:36 +0000

    Added an arrow up

---
+++ lab1/js/arrowUp.js
@@ -0,0 +1,13 @@
+// стрелка наверх
+
+document.write('<div class="arrowUp">');
+document.write('<div>');
+document.write('<a href="#up"><img src="img/arrow.png"></img></a>');            
+document.write('</div>');
+document.write('</div>');
+        
+        // <div class="arrowUp">
+        //     <div>
+        //         <a href="#up"><img src="img/arrow.png"></img></a>            
+        //     </div>
+        // </div>
